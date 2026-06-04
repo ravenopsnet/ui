af2bac027e5ae49ef079082643cbd2509965065c
larbiboukert <larbi.boukert@gmail.com>
2026-06-04 04:12:35 +0100

    Refactor Vite configuration: streamline TypeScript declaration handling and enhance build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import path, { resolve } from "path"
+import { resolve } from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import dts from "vite-plugin-dts"
+import pkg from "./package.json"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,25 +12,40 @@ export default defineConfig({
     tailwindcss(),
     // Automatically compiles your TypeScript declaration files (.d.ts)
     dts({
+      tsconfigPath: "./tsconfig.app.json",
       insertTypesEntry: true,
       include: ["src"],
+      exclude: ["src/main.tsx", "src/App.tsx"],
+      entryRoot: resolve(__dirname, "src"),
+      compilerOptions: {
+        rootDir: resolve(__dirname, "."),
+      },
     }),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  resolve: { tsconfigPaths: true },
   build: {
     lib: {
-      entry: resolve("src/index.ts"),
+      entry: resolve(__dirname, "src/index.ts"),
       name: "RavenUI",
       formats: ["es"],
       fileName: "index",
     },
     rollupOptions: {
       // Prevents React from being bundled into your distribution
-      external: ["react", "react-dom", "react/jsx-runtime"],
+      external: [
+        "react",
+        "react-dom",
+        "react/jsx-runtime",
+        ...Object.keys(pkg.dependencies || {}),
+        ...Object.keys(pkg.peerDependencies || {}),
+        /^@base-ui\/react/, // Ensures sub-imports are externalized
+      ],
+      output: {
+        preserveModules: true,
+        preserveModulesRoot: "src",
+        entryFileNames: "[name].js",
+        assetFileNames: "[name][extname]",
+      },
     },
   },
 })
